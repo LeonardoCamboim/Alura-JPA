4bc601f9d68efde63fab5dce9dba0db197f37350
Leonardo <almleon@gmail.com>
2023-05-25 16:06:36 -0300

    2° Commit

---
+++ src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -0,0 +1,36 @@
+package br.com.alura.loja.teste;
+
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiami redmi");
+        celular.setDescricao("Smartphone");
+        celular.setPreco(new BigDecimal(800));
+
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("LOJA");
+
+        EntityManager em = factory.createEntityManager();
+
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+
+
+
+
+
+
+
+    }
+}
